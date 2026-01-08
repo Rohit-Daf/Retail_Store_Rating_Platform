76a86e37bfaff71447ff33bde5326909c7f539ac
Rohit-Daf <rohitdaph2@gmail.com>
2026-01-08 20:50:56 +0530

    StoreRouter created

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 
 const myAuth = require("./utils/auth");
 const userRouter = require("./routes/userRouter");
+const storeRouter = require("./routes/storeRouter");
 const adminRouter = require("./routes/adminRouter");
+
 const app = express();
 
 app.use(cors());
@@ -11,6 +13,7 @@ app.use(express.json());
 app.use(myAuth);
 
 app.use("/user", userRouter);
+app.use("/store", storeRouter);
 app.use("/admin", adminRouter);
 
 app.listen(4000, "localhost", () => {
