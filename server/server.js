41a0a767197f1fa0fb909fcdba9b699a9a3b01fe
Rohit-Daf <rohitdaph2@gmail.com>
2026-01-08 20:38:22 +0530

    AdminRouter created

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const myAuth = require("./utils/auth");
 const userRouter = require("./routes/userRouter");
-
+const adminRouter = require("./routes/adminRouter");
 const app = express();
 
 app.use(cors());
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(myAuth);
 
 app.use("/user", userRouter);
-
+app.use("/admin", adminRouter);
 
 app.listen(4000, "localhost", () => {
   console.log("server started at port 4000");
